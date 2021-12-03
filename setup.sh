792a95ee118cd05001dad470ed6123da16a6ea3a
Alexander Crawford <mail@alexander-crawford.com>
2021-12-03 20:56:25 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
+ln -sf ~/.dotfiles/.newsboat/config ~/.newsboat/config
+ln -sf ~/.dotfiles/.atom/config.cson ~/.atom/config.cson
+ln -sf ~/.dotfiles/.atom/package.list ~/.atom/package.list
+apm install --packages-file ~/.atom/package.list
+# apm list --installed --bare > ~/.atom/package.list # update package list
