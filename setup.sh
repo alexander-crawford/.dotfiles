d5e2b38ca77336ae47c747b35615636db9139f13
Alexander Crawford <mail@alexander-crawford.com>
2022-09-06 21:35:00 +0200

    remove atom editor config, sad day

--- setup.sh
+++ setup.sh
@@ -2,7 +2,3 @@
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf ~/.dotfiles/.newsboat/config ~/.newsboat/config
-ln -sf ~/.dotfiles/.atom/config.cson ~/.atom/config.cson
-ln -sf ~/.dotfiles/.atom/package.list ~/.atom/package.list
-# apm install --packages-file ~/.atom/package.list # install packages
-# apm list --installed --bare > ~/.atom/package.list # update package list
