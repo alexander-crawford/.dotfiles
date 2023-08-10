ad5d5db592615fa447c681ef9e86ea5092612bc2
Alexander Crawford <mail@alexander-crawford.com>
2023-08-10 21:45:05 +0200

    Remove newsboat as now longer in use

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
-ln -sf ~/.dotfiles/.newsboat/config ~/.newsboat/config
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
