6c9a2754acf2f495e00ad56404b54760361f4809
Alexander Crawford <mail@alexander-crawford.com>
2023-08-10 21:49:19 +0200

    Make a start on working out what dependencies are required on a new install

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# Install dependencies
+sudo apt install i3 -y
+
+# Create sym links
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
