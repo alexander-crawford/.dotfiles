e6f5928f9fc685d800c294db154594ab2586334e
Alexander Crawford <mail@alexander-crawford.com>
2023-12-01 16:41:15 +0100

    Add missing dependencies and global git config

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Install dependencies
-sudo apt install i3 lxappearance zathura sxiv -y
+sudo apt install i3 lxappearance zathura sxiv vim xclip htop tree libreoffice weasyprint php tldr zeal -y
 
 # Create sym links
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
