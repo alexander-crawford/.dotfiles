5781bde190b0620ca1e588bc3064fc20db3beb8f
Alexander Crawford <mail@alexander-crawford.com>
2024-01-27 22:14:32 +0100

    Add live-server to dependencies

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 # Install dependencies
 sudo apt install i3 lxappearance zathura sxiv vim xclip htop tree libreoffice weasyprint php tldr zeal duplicity mpv pass todotxt-cli ncal pwgen thunderbird exiftool entr -y
 
+sudo npm install -g live-server
+
 # Create sym links
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
