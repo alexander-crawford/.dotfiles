8283a7d6a1934e62c5473896f8b5857013064ab1
Alexander Crawford <mail@alexander-crawford.com>
2024-01-27 22:17:52 +0100

    Add Vundle install

--- setup.sh
+++ setup.sh
@@ -11,3 +11,12 @@ mkdir -p ~/.config/i3
 ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+#
+# Download Vundle
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Vundle dependencies
+#sudo apt install build-essential cmake vim-nox python3-dev -y
+#sudo apt install mono-complete golang nodejs default-jdk npm -y
+#cd ~/.vim/bundle/youcompleteme
+#python3 install.py --all
