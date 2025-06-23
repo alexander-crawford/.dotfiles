ff5f356cc023dc730c394ce9c21ed1f85e8c4d4c
Alexander Crawford <alexander.crawford@grafiskdigital.no>
2025-06-23 12:23:28 +0200

    Add dependences and uncomment lines

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install dependencies
-sudo apt install i3 lxappearance zathura sxiv vim xclip htop tree libreoffice weasyprint php tldr zeal duplicity mpv pass todotxt-cli ncal pwgen thunderbird exiftool entr flameshot vim-gtk3 gnome-pomodoro -y
+sudo apt install i3 lxappearance zathura sxiv vim xclip htop tree libreoffice weasyprint php tldr zeal duplicity mpv pass todotxt-cli ncal pwgen thunderbird exiftool entr flameshot vim-gtk3 gnome-shell-pomodoro -y
 
 sudo npm install -g live-server
 
@@ -11,12 +11,12 @@ mkdir -p ~/.config/i3
 ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-#
+
 # Download Vundle
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install Vundle dependencies
-#sudo apt install build-essential cmake vim-nox python3-dev -y
-#sudo apt install mono-complete golang nodejs default-jdk npm -y
-#cd ~/.vim/bundle/youcompleteme
-#python3 install.py --all
+sudo apt install build-essential cmake vim-nox python3-dev -y
+sudo apt install mono-complete golang nodejs default-jdk npm -y
+cd ~/.vim/bundle/youcompleteme
+python3 install.py --all
